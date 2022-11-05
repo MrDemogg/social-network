bfcaca2843e6d888fbd7c95a1e3af47ae68073b5
demog <dg08gg@mail.ru>
2022-11-05 20:41:18 +0300

    router.js / роутер сервера
    к каждому response добавил возвращаемый статус. Добавил возможность изменения профиля. Добавил get запрос, возвращающий все посты, и get запрос, принимающий дату и возвращающий посты с определённой даты.

--- server/router.js
+++ server/router.js
@@ -3,12 +3,42 @@ const router = express.Router()
 const fsHandler = require('./fsHandler/fsHandler')
 
 router.get('/profile', (req, res) => {
-  const answer = fsHandler.getProfile({name: req.body.name, surname: req.body.surname})
+  const answer = fsHandler.getProfile(req.body.name, req.body.surname)
   console.log(answer)
   if (answer) {
-    res.send({login: 'success'})
+    res.status(200).send({login: 'success'})
   } else {
-    res.send({login: 'fail'})
+    res.status(500).send({login: 'fail'})
+  }
+})
+
+router.post('/profile', (req) => {
+  fsHandler.changeProfile(req.body.oldProfile, req.body.newProfile)
+})
+
+router.get('/posts', (_, res) => {
+  const fsResponse = fsHandler.getPosts()
+  if (fsResponse.responseType === 'error') {
+    res.status(500).send(fsResponse.content.message)
+  } else {
+    res.status(200).send(fsResponse.content)
+  }
+})
+
+router.get('/posts/:datetime', (req, res) => {
+  const filterByDate = (arr, start) => {
+    start = start ? new Date(start) : null;
+
+    return arr.filter(({ date }) => {
+      date = new Date(date);
+      return !((start && start > date));
+    });
+  };
+  const fsResponse = fsHandler.getPosts()
+  if (fsResponse.responseType === 'error') {
+    res.status(500).send(fsResponse.content.message)
+  } else {
+    res.status(200).send(filterByDate(fsResponse.content, req.params.datetime))
   }
 })
 
