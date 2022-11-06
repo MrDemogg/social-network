751daa0db8d94bdba49f4935b31074b138ed4fca
demog <dg08gg@mail.ru>
2022-11-06 22:56:11 +0300

    Сервис / SocialService.ts
    Сервис для взаимодействия с сервером, добавил функцию получения постов с сервера, добавил, для облегчения, теги, чтобы посты обновлялись при изменении профиля

--- src/service/SocialService.ts
+++ src/service/SocialService.ts
@@ -1,26 +1,35 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 import {IProfile} from "../models/IProfile";
 import {IProfileChange} from "../models/IProfileChange";
+import {IGetPosts} from "../models/IGetPosts";
 
 export const socialAPI = createApi({
   reducerPath: 'postAPI',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
-  tagTypes: ['Get'],
+  tagTypes: ['Post'],
   endpoints: (build) => ({
     login: build.query<string, IProfile>({
       query: (profile) => ({
         url: '/profile',
         method: 'GET',
         body: profile
-      }),
-      providesTags: result => ['Get']
+      })
     }),
     changeProfile: build.mutation<string, IProfileChange>({
       query: (request) => ({
         url: '/profile',
         method: 'POST',
         body: request
-      })
+      }),
+      invalidatesTags: ['Post']
+    }),
+    fetchPosts: build.query<IGetPosts, IProfile>({
+      query: (profile) => ({
+        url: '/posts',
+        method: 'GET',
+        body: profile
+      }),
+      providesTags: result => ['Post']
     })
   })
 })
