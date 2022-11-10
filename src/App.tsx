4950fd49117144a3e64019c6f03057f402f71fee
demog <dg08gg@mail.ru>
2022-11-10 14:14:08 +0300

    App.tsx
    Добавил проверку на ошибку и логин, от чего будет зависить Alert ошибки и отображение постов, ну и подключил ProfileSettings.tsx с Posts.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import ProfileSettings from "./components/ProfileSettings";
+import {Alert, Card} from "react-bootstrap";
+import Posts from "./components/Posts";
+import {useAppSelector} from "./hooks/redux";
+import {useDispatch} from "react-redux";
+import {socialSlice} from "./store/reducers/SocialSlice";
 
 const App = () => {
+  const {login, errorInfo} = useAppSelector(state => state.socialReducer)
+  const dispatch = useDispatch()
+
+  const closeErrorHandler = () => dispatch(socialSlice.actions.setError({...errorInfo, message: null}))
+
   return (
     <div>
-      
+      <Card>
+        <ProfileSettings />
+        {login
+          ? !errorInfo
+              && <Posts />
+          : <Card.Body><Card.Title style={{textAlign: 'center'}}>You are not logged in</Card.Title></Card.Body>
+        }
+        <Posts />
+      </Card>
+      {errorInfo.message
+        && <Alert variant={"danger"} onClose={closeErrorHandler} dismissible
+          style={{position: 'fixed', width: '100%', top: '50%'}}
+        >
+          <Alert.Heading style={{textAlign: 'center'}}>{errorInfo.message}</Alert.Heading>
+          <p style={{textAlign: 'center'}}>guilty of error: {errorInfo.errorGuilt}</p>
+        </Alert>
+      }
     </div>
   );
 };
